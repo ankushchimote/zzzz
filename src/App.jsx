7e884357532b566f493db60f9bc1f30c2eb5b6f4
Rahul Chimote <87609784+ankushchimote@users.noreply.github.com>
2024-08-24 13:50:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,24 @@ import './App.css'
 import EmployList from './components/EmployList/EmployList'
 import View from './components/View/View'
 import EditModal from './components/EditModal';
+import LeaveRequestForm from './components/LeaveRequestForm';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <>
-   <Router>
-      <Routes>
-        {/* <Route exact path="/" component={EmployList} /> */}
-        <Route path="/" element={<EmployList/>} />
-        <Route path="/edit/:id" element={<EditModal />} />
-      </Routes>
-    </Router>
-   </>
-  // <>
-  // <View/>
-  // </>
+  //  <>
+  //  <Router>
+  //     <Routes>
+        
+  //       <Route path="/" element={<EmployList/>} />
+  //       <Route path="/edit/:id" element={<EditModal />} />
+  //     </Routes>
+  //   </Router>
+  //  </>
+  <>
+  <LeaveRequestForm/>
+  </>
   )
 }
 
