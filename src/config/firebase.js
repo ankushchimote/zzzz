714e954cd30dfb230ed5eb82e84eac7c06b9c387
Rahul Chimote <87609784+ankushchimote@users.noreply.github.com>
2024-08-24 13:05:55 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,12 +8,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyANJ_4nHqUOXvmooNk1sTIBD5TKlwk9J-Q",
-  authDomain: "employ-ab958.firebaseapp.com",
-  projectId: "employ-ab958",
-  storageBucket: "employ-ab958.appspot.com",
-  messagingSenderId: "728632958256",
-  appId: "1:728632958256:web:7f70462e8afa48f7763cf5"
+  apiKey: "AIzaSyC5zHOFlWSZhZVJfTI1bFSQUQ0QnKgHgTU",
+  authDomain: "hrms-9c406.firebaseapp.com",
+  databaseURL: "https://hrms-9c406-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "hrms-9c406",
+  storageBucket: "hrms-9c406.appspot.com",
+  messagingSenderId: "473081956259",
+  appId: "1:473081956259:web:6e7e63372009ae21c2bc16",
+  measurementId: "G-5942P00L1D"
 };
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
